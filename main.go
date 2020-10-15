ce93ac308e35a24249e269b2cf5cbbf2cef76b1d
Zachary Snyder <contact@zsny.dev>
2020-10-15 00:12:42 -0400

    loop through subdirectory and check if recovery needs to be done

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/user"
@@ -12,21 +13,46 @@ func main() {
 
 	// fetch user and host info
 	user, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	host, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 
 	// print user/system info to console for awareness
 	info := fmt.Sprintf("Running as: %s (id: %s)\nHostname: %s \n\n", user.Username, user.Uid, host)
 	fmt.Println(info)
 
+	// obtain slice of image directory
+	subdir, err := ioutil.ReadDir("imagefiles")
+	check(err)
+	// drill down into image directory
+	err = os.Chdir("imagefiles")
+	check(err)
+	fmt.Println("Listing subdirectories... ")
+	// loop over slice of patient image directory and do work
+	for _, entry := range subdir {
+		// drill down into patient directory
+		err = os.Chdir(entry.Name())
+		check(err)
+		// no recovery images unless proven otherwise
+		y := false
+		if _, err := os.Stat("OriginalImages.XVA"); os.IsNotExist(err) {
+			y = true
+		}
+		output := fmt.Sprintf("Found %s | contains recovery images: %v", entry.Name(), y)
+		fmt.Println(output)
+
+		// return up to
+		err = os.Chdir("..")
+		check(err)
+	}
+
 	oldName := "test.txt"
 	newName := "testing.txt"
 	err = os.Rename(oldName, newName)
+	check(err)
+}
+
+func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
